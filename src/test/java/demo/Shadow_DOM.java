c56dbfec3e0364926ec8a996a4bace1d43fa3693
abhilash <abhilashnaidu21@gmail.com>
2024-04-25 19:01:42 +0530

    new chrome options website updated

--- src/test/java/demo/Shadow_DOM.java
+++ src/test/java/demo/Shadow_DOM.java
@@ -11,57 +11,37 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
-public class Shadow_DOM {
+public class demo {
+	static WebDriver driver;
 
-	public static void main(String[] args)throws Exception {
-		WebDriver driver= new ChromeDriver();
+	public static void main(String[] args) throws Exception {
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.get("https://selectorshub.com/iframe-in-shadow-dom/");
-/*
-// first box
-		WebElement root1 =  driver.findElement(By.cssSelector("#userName"));		
-		SearchContext shadow1 = (SearchContext)	((JavascriptExecutor)driver).executeScript("return arguments[0].shadowRoot", root1); 
-		shadow1.findElement(By.cssSelector("#kils")).sendKeys("watermelon");
-
-//second box
-		WebElement frame =   shadow1.findElement(By.cssSelector("#pact1"));
-		driver.switchTo().frame(frame);
-		driver.findElement(By.cssSelector("#glaf")).sendKeys("grapes");
-*/		
-
-/*
-//third box
-		WebElement root2 =  driver.findElement(By.cssSelector("#userName"));		
-		SearchContext shadow2 = (SearchContext)	((JavascriptExecutor)driver).executeScript("return arguments[0].shadowRoot", root2); 
 		
-		WebElement root3 =  shadow2.findElement(By.cssSelector("#app2"));		
-		SearchContext shadow3 = (SearchContext)	((JavascriptExecutor)driver).executeScript("return arguments[0].shadowRoot", root3); 
-		shadow3.findElement(By.cssSelector("#pizza")).sendKeys("pineapple");
-*/		
-
-/*		
-//fourth box		
+		WebElement root1 = driver.findElement(By.cssSelector("#userName"));
+		SearchContext s1 = (SearchContext) ((JavascriptExecutor) driver).executeScript("return arguments[0].shadowRoot",root1);
+		s1.findElement(By.cssSelector("#kils")).sendKeys("abhilash");
 		
-		WebElement root4 =  driver.findElement(By.cssSelector("#userName"));		
-		SearchContext shadow4 = (SearchContext)	((JavascriptExecutor)driver).executeScript("return arguments[0].shadowRoot", root4); 
-	
-		shadow4.findElement(By.cssSelector("#concepts")).click();
+		driver.switchTo().frame(s1.findElement(By.cssSelector("#pact1")));
+		driver.findElement(By.cssSelector("#glaf")).sendKeys("abhilash");
+		
+		driver.switchTo().parentFrame();
+		
+		WebElement root2 = s1.findElement(By.cssSelector("#app2"));
+		SearchContext s2 = (SearchContext) ((JavascriptExecutor) driver).executeScript("return arguments[0].shadowRoot",root2);
+		s2.findElement(By.cssSelector("#pizza")).sendKeys("abhilash");
+		
+		s1.findElement(By.cssSelector("#concepts")).click();
 		Actions a = new Actions(driver);
+		
 		a.keyDown(Keys.TAB).perform();
-		a.sendKeys("CusturdApple").perform();
-*/	
-
-	
-//fifthbox -- shadow closed
-//		If shadow dom is closed then we must move to top nearby element and click
-		//	and move our tab to box by using actions class and send keys in actions class	
-			
+		a.sendKeys("abhilash").perform();
 		
 		driver.findElement(By.cssSelector("#userPass")).click();
-		Actions a = new Actions(driver);
 		a.keyDown(Keys.TAB).perform();
-		a.sendKeys("Faluda").perform();
-	
+		a.sendKeys("abhilash").perform();
+		
 	}
-
 }
