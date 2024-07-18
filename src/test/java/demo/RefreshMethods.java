64b6a7eaa1992978a1e388e643a9c4914e00afd9
abhilash <abhilashnaidu21@gmail.com>
2024-07-18 07:01:24 +0530

    Refresh Methods

--- src/test/java/demo/RefreshMethods.java
+++ src/test/java/demo/RefreshMethods.java
@@ -1,20 +1,55 @@
 package demo;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
+import java.net.URI;
+
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class RefreshMethods {
 public static void main(String[] args)throws Exception {
+	
+	
 	WebDriver driver = new ChromeDriver();
 	driver.get("https://www.google.com");
-	Thread.sleep(2000);
+	
+	//Method 1
+	
 	driver.navigate().refresh();
-	Thread.sleep(2000);
-	driver.navigate().to("https://www.google.com");
-	Thread.sleep(2000);
+	
+	//Method 2
+	
+	driver.get(driver.getCurrentUrl());
+	
+
+	//Method 3
+	
 	driver.navigate().to(driver.getCurrentUrl());
-	driver.findElement(By.name("q")).sendKeys(Keys.F5);
+	
+
+	//Method 4
+	
+	driver.navigate().to(new URI(driver.getCurrentUrl()).toURL());
+			
+			
+
+	//Method 5
+	
+	Robot r = new Robot();
+	r.keyPress(KeyEvent.VK_CONTROL);
+	r.keyPress(KeyEvent.VK_R);
+	
+	r.keyRelease(KeyEvent.VK_CONTROL);
+	r.keyRelease(KeyEvent.VK_R);
+	
+	
+	//Method 6
+	
+	((JavascriptExecutor)driver).executeScript("window.location.reload(true)"); 
+	
+
+	
 }
 }
